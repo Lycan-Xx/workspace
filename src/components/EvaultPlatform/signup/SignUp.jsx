9caadacda207879b44f7781be2be1c42a49c6291
Mohammad (Sani) Bello <69519271+Lycan-Xx@users.noreply.github.com>
2025-07-22 10:38:42 +0100

    Updated SignUp.jsx

--- src/components/EvaultPlatform/signup/SignUp.jsx
+++ src/components/EvaultPlatform/signup/SignUp.jsx
@@ -249,7 +249,7 @@ function OTPVerificationStep({ onSubmit, onBack }) {
               !isPhoneVerified || otp.join("").length !== 6
                 ? "bg-gray-400 cursor-not-allowed"
                 : "bg-[#025798] hover:bg-[#025798]/90"
-            } text-white rounded-lg font-medium bg-[#025798] hover:bg-white text-white hover:text-[#025798] transition duration-300 border-2 border-[#025798] rounded-lg ease-linear`}
+            } text-white rounded-lg font-medium border-2 border-[#025798] rounded-lg ease-linear`}
           >
             Continue
           </button>
